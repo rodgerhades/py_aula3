5ed3aaa290fb1f303a348a8bd2038a641e3f664f
Rodger Cabrerisso <83619204+rodgerhades@users.noreply.github.com>
2024-02-05 19:06:50 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+num1 = 5
+num2 = 3.5
+
+a = float(num1)
+b = int (num2)
+
+(num1 + num2)
+(num1 - num2)
+(num1 * num2)
+(num1 / num2)
+(num1 ** num2)
+(num1 // num2)
+
+(3 + 5 * 7 + 3)
+((3 + 5) * (7 + 3))
+
+print(abs(-15))
+print(pow(3,3))
+print(max(1,5,85,-55,74))
+print(min(1,5,85,-55,74))
+
+print(round(8.7))
+
+import math
+print(math.floor(8.8))
+print(math.ceil(8.0001))
+print(math.sqrt(9))
